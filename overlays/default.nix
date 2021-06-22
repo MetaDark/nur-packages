9edfe3f8f1787ad919f2e9cb47dc4fe80647d216
Kira Bruneau <kira.bruneau@pm.me>
2021-06-21 23:25:49 -0400

    flake.nix: replace `legacyPackages` with `packages`

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,5 +1,26 @@
 {
-  nur = final: prev: import ../pkgs {
-    pkgs = prev;
-  };
+  default = final: prev:
+    let
+      pythonOverrides = removeAttrs (import ../development/python-modules final) [ "callPackage" ];
+    in
+    (removeAttrs (import ../pkgs final prev) [ "callPackage" ]) // {
+      python = prev.python.override { packageOverrides = pythonOverrides; };
+      python2 = prev.python2.override { packageOverrides = pythonOverrides; };
+      python3 = prev.python3.override { packageOverrides = pythonOverrides; };
+      python27 = prev.python27.override { packageOverrides = pythonOverrides; };
+      python36 = prev.python36.override { packageOverrides = pythonOverrides; };
+      python37 = prev.python37.override { packageOverrides = pythonOverrides; };
+      python38 = prev.python38.override { packageOverrides = pythonOverrides; };
+      python39 = prev.python39.override { packageOverrides = pythonOverrides; };
+      python310 = prev.python310.override { packageOverrides = pythonOverrides; };
+      pythonPackages = final.python.pkgs;
+      python2Packages = final.python2.pkgs;
+      python3Packages = final.python3.pkgs;
+      python27Packages = final.python27.pkgs;
+      python36Packages = final.python36.pkgs;
+      python37Packages = final.python37.pkgs;
+      python38Packages = final.python38.pkgs;
+      python39Packages = final.python39.pkgs;
+      python310Packages = final.python310.pkgs;
+    };
 }
